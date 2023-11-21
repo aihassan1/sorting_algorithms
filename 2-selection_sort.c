e68cdca74a00b68b58b1bcc888cf9d216010a42b
aihassan1 <abdosruor57@gmail.com>
2023-11-21 08:23:49 +0000

    AAAA

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -27,6 +27,9 @@ void swap(int *a, int *b)
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index;
+    
+    if (array == NULL || size < 2)
+	   return;
 
 	for (i = 0; i < size; i++)
 	{
